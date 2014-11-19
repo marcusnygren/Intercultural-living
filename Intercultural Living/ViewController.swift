7ac4695dda40e5a1a6dbc0e5e611bda1e4d70175
marny568 <marny568@student.liu.se>
2014-11-19 16:58:24 +0100

    Init

---
+++ Intercultural Living/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intercultural Living
+//
+//  Created by Marcus Nygren on 2014-11-19.
+//  Copyright (c) 2014 Intercultural Living AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
