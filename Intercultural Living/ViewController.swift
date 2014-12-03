bf0b6b9fb4a991a2644c99e3248092be901f0b9e
Tomas Walander <twalander@gmail.com>
2014-12-03 16:27:22 +0100

    Added functionality to calculate where on the image the user has tapped

--- Intercultural Living/ViewController.swift
+++ Intercultural Living/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var myLabel: UILabel!
     
+    @IBOutlet weak var apartmentImage: UIImageView!
+    @IBOutlet weak var xPos: UILabel!
+    @IBOutlet weak var yPos: UILabel!
+    @IBOutlet weak var xPercentage: UILabel!
+    @IBOutlet weak var yPercentage: UILabel!
+    
+    let tap = UITapGestureRecognizer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //init tap
+        tap.addTarget(self, action: "tappedView")
+        apartmentImage.addGestureRecognizer(tap)
+        apartmentImage.userInteractionEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tappedView() {
+        //get tapping positiong
+        var pos : CGPoint = tap.locationInView(apartmentImage)
+        var x : CGFloat = pos.x
+        var y : CGFloat = pos.y
+        
+        //get image information
+        var imageBounds : CGRect = apartmentImage.bounds
+        var image : UIImage = apartmentImage.image!
+        var imageScale : CGFloat = image.scale
+
+        //calculate image width and height in pixels from bounds and scale
+        var width : CGFloat = imageBounds.size.width * imageScale
+        var height : CGFloat = imageBounds.size.height * imageScale
+        
+        //calculate tapping position in percentage of image width and height
+        var xPer : CGFloat = x / width
+        var yPer : CGFloat = y / height
+        
+        //print stuff for now
+        xPos.text = pos.x.description
+        yPos.text = pos.y.description
+        xPercentage.text = xPer.description
+        yPercentage.text = yPer.description
     }
 
 
