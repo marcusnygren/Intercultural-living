5db073e82cebb8eda512f672a01b14de53bd6c22
Tomas Walander <twalander@gmail.com>
2014-12-04 15:03:32 +0100

    Added mHome screen with options like apartment, migration office etc.

---
+++ Intercultural Living/CustomHomeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomHomeCollectionViewCell.swift
+//  Intercultural Living
+//
+//  Created by Tomas Walander on 04/12/14.
+//  Copyright (c) 2014 Intercultural Living AB. All rights reserved.
+//
+
+import UIKit
+
+class CustomHomeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ Intercultural Living/HomeController.swift
@@ -0,0 +1,48 @@
+//
+//  HomeController.swift
+//  Intercultural Living
+//
+//  Created by Tomas Walander on 04/12/14.
+//  Copyright (c) 2014 Intercultural Living AB. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+
+    var icons:[String] = []
+    var modules:[String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        icons = [ "icon_home_migration_office.png" , "icon_home_work_office.png" , "icon_home_living.png" , "icon_home_sweden.png" , "icon_home_other.png" ]
+        modules = [ "" , "" , "apartmentView" , "", "" ]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func collectionView(collectionView: UICollectionView,numberOfItemsInSection section: Int) -> Int {
+        return icons.count
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView!) -> Int{
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView,cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell : CustomHomeCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("homeCollectionViewCell", forIndexPath: indexPath) as CustomHomeCollectionViewCell
+        cell.imageView.image=UIImage(named: icons[indexPath.row]) //UIImage(named: icons[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView!, didSelectItemAtIndexPath indexPath: NSIndexPath!) {
+//        var goto : String = modules[indexPath.row]
+//        if ( goto != "" ) {
+//            let stoveMainView = self.storyboard?.instantiateViewControllerWithIdentifier(goto) as StoveController
+//            self.navigationController?.pushViewController(stoveMainView, animated: true)
+//        }
+    }
+    
+}
